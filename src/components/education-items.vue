4d1e94cb7b8326ce71858d0a4a6a9202e58bd090
vibovenkat123 <vibovenkat@gmail.com>
2022-09-25 14:28:52 -0500

    fix word wrap

--- src/components/education-items.vue
+++ src/components/education-items.vue
@@ -5,10 +5,10 @@ const store = useInfoStore();
 </script>
   <template>
     <ul v-for="education in store.education" :key="education.id">
-      <div class="flex mb-2">
+      <div class="flex mb-2 justify-between">
         <div class="font-semibold text-lg">{{education.from}} - {{education.to}}</div>
-        <div class="flex flex-col ml-9">
-          <div class="font-semibold text-lg">{{education.degree}}</div>
+        <div class="flex flex-col w-1/2">
+          <div class="font-semibold text-lg break-words">{{education.degree}}</div>
           <div class="italic mt-3 mb-3">{{education.location}}</div>
         </div>
       </div>
--- src/components/experience-items.vue
+++ src/components/experience-items.vue
@@ -5,9 +5,9 @@ const store = useInfoStore();
 </script>
 <template>
   <ul v-for="experience in store.experiences" :key="experience.id">
-    <div class="flex mb-2">
+    <div class="flex mb-2 justify-between">
       <div class="font-semibold text-lg">{{experience.from}} - {{experience.to}}</div>
-      <div class="flex flex-col ml-9">
+      <div class="flex flex-col w-1/2">
         <div class="font-semibold text-lg">{{experience.title}}</div>
         <div class="italic mt-3 mb-3">{{experience.location}}</div>
         <div>{{experience.description}}</div>
