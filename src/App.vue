76a45f6b00e37863cdd4745b9db6175a12bc3d00
vibovenkat123 <vibovenkat@gmail.com>
2022-09-25 18:40:34 -0500

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 import CvMain from './components/cv-main.vue';
 import InputMain from './components/input-main.vue';
+import HeaderItem from './components/header-item.vue';
 </script>
 <template>
-  <div class="flex">
-    <InputMain/>
-    <CvMain></CvMain>
+  <div class="flex flex-col">
+    <HeaderItem/>
+    <div class="flex">
+      <InputMain/>
+      <CvMain></CvMain>
+  </div>
   </div>
 </template>
---
+++ src/components/header-item.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-500 flex justify-center items-center h-24 flex-col">
+    <span class="text-white text-3xl">CV-APP</span>
+    <div class="flex items-center">
+      <span class="text-white text-3xl">Made With Vue</span>
+      <img src="https://vuejs.org/images/logo.png" class  = "w-12" alt="">
+    </div>
+  </div>
+</template>
