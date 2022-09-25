ab89272888859ca6227150928e63936adce9b5ec
vibovenkat123 <vibovenkat@gmail.com>
2022-09-25 12:15:50 -0500

    Setup vue, eslint and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,4 @@
+<script></script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1 class="text-3xl">header</h1>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './css/output/output.css';
 
 createApp(App).mount('#app');
