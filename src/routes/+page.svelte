72fb6ba1a7995fc06bde8dd4bc4f459ae2728716
vibovenkat123 <vibovenkat@gmail.com>
2022-10-02 14:21:44 -0500

    rebuild app in svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Header from "../components/header.svelte";
+  import Input from "../components/input.svelte";
+  import Cv from "../components/cv.svelte";
+</script>
+
+<main>
+  <div class="flex flex-col">
+    <Header />
+    <div class="flex-col flex xl:flex-row">
+      <Input />
+      <Cv />
+    </div>
+  </div>
+</main>
